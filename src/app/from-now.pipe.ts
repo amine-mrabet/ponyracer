ea878316650ee6a25580bceb925981452a9fc600
X415795 <mamrabet@vermeg.com>
2024-10-02 13:32:03 +0100

    Custom pipe with date-fns

---
+++ src/app/from-now.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistanceToNowStrict, parseISO } from 'date-fns';
+
+@Pipe({
+  name: 'fromNow',
+  standalone: true
+})
+export class FromNowPipe implements PipeTransform {
+  transform(value: string, ...args: unknown[]): string {
+    return formatDistanceToNowStrict(parseISO(value), { addSuffix: true });
+  }
+}
