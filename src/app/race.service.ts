425eb670f3f1e2723b2aea4ca4df0c1c36844f87
Mohamed Amine Mrabet <mamrabet@vermeg.com>
2024-10-03 13:28:41 +0100

    HTTP

--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -1,38 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { RaceModel } from './models/race.model';
-import { delay, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RaceService {
   constructor() {}
+  http = inject(HttpClient);
   list(): Observable<Array<RaceModel>> {
-    return of([
-      {
-        id: 12,
-        name: 'Paris',
-        ponies: [
-          { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-          { id: 2, name: 'Big Soda', color: 'ORANGE' },
-          { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-          { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-          { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
-        ],
-        startInstant: '2024-09-18T08:02:00Z'
-      },
-      {
-        id: 13,
-        name: 'Tokyo',
-        ponies: [
-          { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-          { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-          { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-          { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-          { id: 10, name: 'Great Soda', color: 'ORANGE' }
-        ],
-        startInstant: '2024-10-01T08:03:00Z'
-      }
-    ]).pipe(delay(500));
+    const params = { status: 'PENDING' };
+    return this.http.get<Array<RaceModel>>('https://ponyracer.ninja-squad.com/api/races', { params });
   }
 }
