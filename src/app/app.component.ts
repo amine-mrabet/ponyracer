5af1ae7d3215e02afb7784e3b326a383ef90d337
Mohamed Amine Mrabet <mamrabet@vermeg.com>
2024-10-03 17:55:33 +0100

    Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { RacesComponent } from './races/races.component';
 
 @Component({
   selector: 'pr-root',
   standalone: true,
-  imports: [MenuComponent, RacesComponent],
+  imports: [MenuComponent, RacesComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ponyracer';
-}
+export class AppComponent {}
