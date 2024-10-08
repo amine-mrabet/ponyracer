d3770b6bc8d9e79d89d1c0cb16fb2bb183d95f96
Mohamed Amine Mrabet <mamrabet@vermeg.com>
2024-10-08 17:34:40 +0100

    Remember me

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,14 +9,28 @@ import { UserModel } from './models/user.model';
 export class UserService {
   http = inject(HttpClient);
   userEvents = new BehaviorSubject<UserModel | null>(null);
-  authenticate(login: string, password: string): Observable<UserModel> {
-    return this.http
-      .post<UserModel>('https://ponyracer.ninja-squad.com/api/users/authentication', { login, password })
-      .pipe(tap((user: UserModel) => this.userEvents.next(user)));
+  constructor() {
+    this.retrieveUser();
   }
-
   register(login: string, password: string, birthYear: number): Observable<UserModel> {
     const body = { login, password, birthYear };
     return this.http.post<UserModel>('https://ponyracer.ninja-squad.com/api/users', body);
+  }
+
+  authenticate(login: string, password: string): Observable<UserModel> {
+    return this.http
+      .post<UserModel>('https://ponyracer.ninja-squad.com/api/users/authentication', { login, password })
+      .pipe(tap((user: UserModel) => this.storeLoggedInUser(user)));
+  }
+  private storeLoggedInUser(user: UserModel): void {
+    localStorage.setItem('rememberMe', JSON.stringify(user));
+    this.userEvents.next(user);
+  }
+  retrieveUser(): void {
+    const value = window.localStorage.getItem('rememberMe');
+    if (value) {
+      const user = JSON.parse(value) as UserModel;
+      this.userEvents.next(user);
+    }
   }
 }
